b28938595a5825efa98c5577f4edb5ad46465b42
Miguel Piña <miguelepq@gmail.com>
2020-10-31 05:29:17 -0300

    init

---
+++ src/main/java/com/apweif/app/inventariodolar/InventariodolarApplication.java
@@ -0,0 +1,13 @@
+package com.apweif.app.inventariodolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventariodolarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventariodolarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apweif/app/inventariodolar/InventariodolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apweif.app.inventariodolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventariodolarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
