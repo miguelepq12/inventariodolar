8f8c304d2bf1b00d3af277f68a7878096d06dafb
Miguel Piña <miguelepq@gmail.com>
2020-11-01 15:26:18 -0300

    improve test

--- src/main/java/com/apweif/app/inventariodolar/entity/User.java
+++ src/main/java/com/apweif/app/inventariodolar/entity/User.java
@@ -1,9 +1,7 @@
 package com.apweif.app.inventariodolar.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 public class User {
--- src/test/java/com/apweif/app/inventariodolar/InventariodolarApplicationTests.java
+++ src/test/java/com/apweif/app/inventariodolar/InventariodolarApplicationTests.java
@@ -3,8 +3,7 @@ package com.apweif.app.inventariodolar;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class InventariodolarApplicationITests {
+class InventariodolarApplicationTests {
 
     @Test
     void contextLoads() {
